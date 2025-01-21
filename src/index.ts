018dc3f50b8787c1feee63022029fb7933ffd8d3
Sergey Volkov <js2me@outlook.com>
2025-01-21 17:23:21 +0300

    feat: add Container export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable @typescript-eslint/no-use-before-define */
 
-import { rootContainer } from './container.js';
+import { rootContainer, Container } from './container.js';
+import { ContainerConfig } from './container.types.js';
 import { Tag } from './tag.js';
 
 export const container = rootContainer;
@@ -10,3 +11,6 @@ export const inject = container.inject.bind(container);
 export const register = container.register.bind(container);
 export const destroy = container.destroy.bind(container);
 export const tag = Tag.create;
+
+export { Container };
+export type { ContainerConfig };
