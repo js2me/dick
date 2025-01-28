dd193eb4a7ef0bb89a6ca9fce7b7549d9306d537
Sergey Volkov <js2me@outlook.com>
2025-01-29 01:10:48 +0300

    feat: improve inject decorator

--- src/decorators/inject.ts
+++ src/decorators/inject.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Class, IsEmptyArray } from 'yummies/utils/types';
 
-import { container } from '../container.js';
+import { Container, container } from '../container.js';
 import { Tag } from '../tag.js';
 
 export function inject<TTarget, TArgs extends any[] = []>(
@@ -13,16 +13,19 @@ export function inject<TTarget, TArgs extends any[] = []>(
   ..._: IsEmptyArray<TArgs> extends true ? [getArgs?: (ctx: any) => TArgs] : []
 ): any;
 export function inject(value: any, ...injectArgs: any[]) {
+  const usageContainer: Container =
+    // @ts-ignore
+    this instanceof Container ? this : container;
   return (...args: any[]): any => {
     if (args.length === 2) {
       if (args[1]?.kind) {
         return function inializer() {
           // @ts-ignore
-          return container.inject(value, injectArgs[0]?.(this));
+          return usageContainer.inject(value, injectArgs[0]?.(this));
         };
       } else if (typeof args[0] === 'object' && typeof args[1] === 'string') {
         // @ts-ignore
-        args[0][args[1]] = container.inject(value, injectArgs[0]?.(this));
+        args[0][args[1]] = usageContainer.inject(value, injectArgs[0]?.(this));
       } else {
         throw new TypeError('not implemented');
       }
